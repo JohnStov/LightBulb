3109fceeed00c3f7c1c9903590b53ff0ce7390f8
John Stovin <john.d.stovin@gmail.com>
2015-11-06 17:59:47 +0000

    Led on/off

--- LightBulb/Bulb.xaml.cs
+++ LightBulb/Bulb.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
@@ -7,9 +9,19 @@ namespace LightBulb
 {
     public sealed partial class Bulb : UserControl
     {
+        private GpioPin ledPin;
+        private const int LedGpio = 5;
+
         public Bulb()
         {
             InitializeComponent();
+
+            Loaded += (sender, args) =>
+            {
+                var controller = GpioController.GetDefault();
+                ledPin = controller.OpenPin(LedGpio);
+                ledPin.SetDriveMode(GpioPinDriveMode.Output);
+            };
         }
 
         public static readonly DependencyProperty IsOnProperty = DependencyProperty.Register("IsOn", typeof(bool), typeof(Bulb), new PropertyMetadata(false, OnIsOnChanged));
@@ -18,8 +30,12 @@ namespace LightBulb
         {
             var bulb = (Bulb) o;
             var newValue = (bool) e.NewValue;
+            if (newValue == bulb.IsOn)
+                return;
+
             bulb.BulbOff.Visibility = newValue ? Visibility.Collapsed : Visibility.Visible;
             bulb.BulbOn.Visibility = newValue ? Visibility.Visible : Visibility.Collapsed;
+            bulb.ledPin.Write(newValue ? GpioPinValue.High : GpioPinValue.Low);
         }
 
         public bool IsOn
