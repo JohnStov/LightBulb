fd177f61aa7005e41b92c97f98b6c1f033c9d245
John Stovin <john.d.stovin@gmail.com>
2015-11-06 10:07:08 +0000

    Toggleable lightbulb

---
+++ LightBulb/Bulb.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace LightBulb
+{
+    public sealed partial class Bulb : UserControl
+    {
+        public Bulb()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty IsOnProperty = DependencyProperty.Register("IsOn", typeof(bool), typeof(Bulb), new PropertyMetadata(false, OnIsOnChanged));
+
+        private static void OnIsOnChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var bulb = (Bulb) o;
+            var newValue = (bool) e.NewValue;
+            bulb.BulbOff.Visibility = newValue ? Visibility.Collapsed : Visibility.Visible;
+            bulb.BulbOn.Visibility = newValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public bool IsOn
+        {
+            get { return (bool) GetValue(IsOnProperty); }
+            set { SetValue(IsOnProperty, value); }
+        }
+    }
+}
